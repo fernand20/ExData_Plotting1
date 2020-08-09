593043dcffe164a710f2e36e173507b9c1445148
fernand20 <68525955+fernand20@users.noreply.github.com>
2020-08-08 19:24:47 -0500

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,10 @@
+#Reading, naming and subsetting power consumption data
+dataFile <- "Data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#calling the basic plot function
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
